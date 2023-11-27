f13ed27e556a4054b8c6f25616b38e793d67f3c0
MacriFabiane <mariacristinafabiane@gmail.com>
2023-11-27 15:03:47 -0300

    .

--- Humano.java
+++ Humano.java
@@ -4,22 +4,67 @@ public class Humano extends Jogador implements JogarComoHumano{
     private String conta;
     private int numeroBanco;
 
-    public Humano(String nome, String tipoJogador, String cpf){
-        super(nome, tipoJogador);
+    public Humano(String nome, String tipoJogador, String cpf,double saldo){
+        super(nome, tipoJogador, saldo);
         this.cpf = cpf;
         this.agencia = "2567-8";
         this.conta = "2564987";
-        this.numeroBanco = 007;
+        this.numeroBanco = 007;    
     }
 
     @Override
-    public int escolherJogada(JogoGeneral Jogo) {
-        return 0;
+    public int escolherJogada(JogoGeneral Jogo) { //vai escolher a jogada do jogo general
+        int opcao=0;
+             do {
+                System.out.println("Para qual jogada deseja marcar: [1-13] " + players[i].getNome() + "?");
+                System.out.println("1\t2\t3\t4\t5\t6\t7(T)\t8(Q)\t9(F)\t10(S+)\t11(S-)\t12(G)\t13(X)");
+                players[i].mostrarJogadasExecutadas();
+
+                while(opcao<=0|| opcao>13 || players[i].getJogo().getJogadas(opcao-1)!=-1){//caso o usuário tente escolher uma opcao inexistente ou alguma jogada já feita de novo
+                    opcao = teclado.nextInt();
+                    teclado.nextLine(); // Limpar o buffer de entrada após a leitura do inteiro
+                   
+                    if(opcao<=0||opcao>13 || players[i].getJogo().getJogadas(opcao-1)!=-1){
+                         System.out.println("Jogada inválida, escolha outra.");
+                    }
+                    }
+
+                     if (players[i].getJogo().getJogadas(opcao-1)==-1) { //se a jogada ainda nao tiver sido feita
+                        this.players[i].getJogo().setJogadas(opcao-1, this.players[i].getJogo().pontuarJogada(opcao));
+                    } 
+                } while (players[i].getJogo().getJogadas(opcao-1)==-1);
+      return 0;
     }
 
     @Override
     public int escolherJogo() {
-        
+          int opcao = 0;
+          if(players.saldo <=0.0){
+            System.out.println("Saldo insufíciente");
+            return 0;
+          }
+          if(players.jogosRealizados>=10){
+            System.out.println("Você atingiu o limite máximo de jogatina no Cassino M&M, volte amanhã!");
+            return 0;
+          }
+        do{
+            System.out.println("Qual jogo deseja jogar?");//DEFINE QUAL JOGO É
+            System.out.println("1 - Jogo General");
+            System.out.println("2 - Jogo Azar");
+            opcao = teclado.nextInt();
+            teclado.nextLine();
+
+            if(opcao==1){
+                    //função que inicia general
+            }
+            else if(opcao==2){
+                //função que inicia o jogo de azr
+            }
+            else{
+                System.out.println("Opção inválida!");
+                opcao = 0;
+            }
+        }while(opcao==1||opcao==2);
         return 0;
     }
 }
--- Maquina.java
+++ Maquina.java
@@ -1,15 +1,46 @@
+import java.util.Random;
 public class Maquina extends Jogador implements JogarComoMaquina {
-    private Jogador players = new Jogador();
-    private JogoDados jDados = new JogoDados();
+    // private Jogador players = new Jogador();
+    // private JogoDados jDados = new JogoDados();
 
-    public Maquina(String nome, String tipoJogador){
-        super(nome, tipoJogador);
+    public Maquina(String nome, String tipoJogador, double saldo){
+        super(nome, tipoJogador, saldo);
     }
+
+    public int sorteiaJogo() { //como cada jogo é individual e nehum jogador joga com outro é necessário um método pra maquina "entrar" em um jogo
+        int opcao = 0;
+      if(players.saldo <=0.0){
+        System.out.println("Saldo insufíciente");
+        return 0;
+      }
+      if(players.jogosRealizados>=10){
+        System.out.println("Você atingiu o limite máximo de jogatina no Cassino M&M, volte amanhã!");
+        return 0;
+      }
+      do{
+          System.out.println("Qual jogo deseja jogar?");//DEFINE QUAL JOGO É
+          System.out.println("1 - Jogo General");
+          System.out.println("2 - Jogo Azar");
+          opcao = random.nextInt(2)+1; //sorteia 1 ou 2 pra máquina jogar
+
+          if(opcao==1){
+                  //função que inicia general
+          }
+          else if(opcao==2){
+              //função que inicia o jogo de azar
+          }
+          else{
+              System.out.println("Opção inválida!");
+              opcao = 0;
+          }
+          jogosRealizados++;
+      }while((opcao==1||opcao==2)&& this.saldo>0 && this.jogosRealizados<10); //   BOTAR MAIS UM EE PRA QUANDO JÁ TIVER SIDO JOGADO 10 JOGOS
+      return 0;
+  }
 
     @Override
     public int aplicarEstrategia() {
-        if (players[i].getTipoJogador().equals("M")|| players[i].getTipoJogador().equals("m") && jDados.nomeJogo){   // Se for do tipo máquina irá escolher a melhor jogada
-            opcao=0;
+        int opcao=0;
             int melhorPontuacao = 0;
             int melhorJogada=0;
                 while(opcao<13){//basicamente vai ver para aquela rodada qual vai ser a jogada com maior pontuação
@@ -36,7 +67,7 @@ public class Maquina extends Jogador implements JogarComoMaquina {
             System.out.println("1\t2\t3\t4\t5\t6\t7(T)\t8(Q)\t9(F)\t10(S+)\t11(S-)\t12(G)\t13(X)");
             players[i].mostrarJogadasExecutadas();
             System.out.println("Jogada que a maquina escolheu: "+ (melhorJogada+1));//retorna a jogada feita pela maquina melhorjogada(posição do vet)+1(pra ficar o "nome" da jogada certinho)
-        }
+        
         return 0;
     }
 }
