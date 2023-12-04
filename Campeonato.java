22790dd95b5944d8f78ffb3dbcaf5bcd433e4c83
MacriFabiane <mariacristinafabiane@gmail.com>
2023-12-04 18:12:07 -0300

    FINNNNAAAAL

--- Campeonato.java
+++ Campeonato.java
@@ -739,7 +739,7 @@ public class Campeonato implements Serializable {
                                     }
                                 }
                                 
-                            };
+                            }
                     }
                         
                     break;
